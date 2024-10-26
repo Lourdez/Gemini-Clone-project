1e0da70b4a0b5dc9b9234215bb640c3b6e829616
Rajnag24324 <nagarajanindirathi7991@gmail.com>
2024-10-26 19:07:54 +0530

    Context text change

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,8 @@
 import React from 'react'
 import './App.css'
 import Sidebar from './component/sidebar'
-import MainContent from './component/mainContent'
-// import Home from './pages/Home/Home'
-// import { Routes, Route, Link } from 'react-router-dom'
-// import Login from './pages/Login/Login'
-// import LandingPage from './pages/LandingPage/LandingPage'
+// import MainContent from './component/MainContent'
+
 
 
 
@@ -14,13 +11,7 @@ import MainContent from './component/mainContent'
 const App = () => {
   return (
     <>
-    {/* <Routes>
-
-      <Route path='/' element={ <LandingPage />} />
-      <Route path='/home' element={ <Home />} />
-      <Route path='/login' element={ <Login />} />
-
-    </Routes> */}
+   
 
     <Sidebar />
     
